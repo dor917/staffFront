49b264c77d36d7e4a866ab41d9d415584d49f9b9
donha <donha917@naver.com>
2021-10-06 19:05:13 +0900

    채팅 js 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Chat from "./pages/Chat.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+        <Link to="/Chat">
+          <button>김돈하</button>
+        </Link>
+        <Route exact path="/Chat" component={Chat} />
+      </BrowserRouter>
     </div>
+
   );
 }
 
