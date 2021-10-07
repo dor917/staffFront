e6efb7df27dccff200e21c4f889611ebcba91f9f
donha <donha917@naver.com>
2021-10-07 16:24:50 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import Chat from "./pages/Chat.js";
 
 
