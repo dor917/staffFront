5cb1b1e97a8e653761fb8ddc5707c318d355b960
donha <donha917@naver.com>
2021-10-09 21:26:03 +0900

    사이드바 수정

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { BrowserRouter, Route} from "react-router-dom";
 import Chat from "./pages/Chat.js";
 import Header from "./pages/Header.js";
 import FileList from "./pages/FileList.js";
-<<<<<<< HEAD
 import Sidebar from "./pages/Sidebar.js";
-=======
 import Calendar from "./pages/Calendar.js";
 
->>>>>>> 99b6cbff05117c77852177f9ecc1a22f5b61f89a
 
 function App() {
   return (
@@ -18,11 +15,8 @@ function App() {
       <BrowserRouter>
         <Route exact path="/Chat" component={Chat} />
         <Route exact path="/FileList" component={FileList} />
-<<<<<<< HEAD
         <Route exact path="/Sidebar" component={Sidebar} />
-=======
         <Route exact path="/Calendar" component={Calendar} />
->>>>>>> 99b6cbff05117c77852177f9ecc1a22f5b61f89a
       </BrowserRouter>
     </div>
 
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,50 +1,37 @@
 import React from "react";
 import '../css/bootstrap.min.css'
+import '../css/sidebar.css'
 
 function Sidebar() {
     return(
-            <div className="Sidebar"> 
-                <div className="logo_container">  {/*로고*/}
-                    <div className="logo"> 
-                        <box-icon name='navigation'></box-icon>
-                            <div className="logo_Name">navigation</div>                   
-                    </div>
-                    <div className="Sidebar_menu">   {/*사이드바 메뉴*/}
-                        <box-icon name='chevrons-right'id="btn"></box-icon>
-                    </div>
-                    <ul className="nav_list">   {/*사이드바 리스트*/}
-                        <li >
-                            <a href="#">
-                             <box-icon name='bomb'></box-icon>
-                                <span className="links_name">Dashboard</span>
-                            </a>
-                        </li>
-                        <li >
-                            <a href="#">
-                             <box-icon name='bomb'></box-icon>
-                                <span className="links_name">Dashboard2</span>
-                            </a>
-                        </li><li >
-                            <a href="#">
-                             <box-icon name='bomb'></box-icon>
-                                <span className="links_name">Dashboard3</span>
-                            </a>
-                            <a href="#">
-                                <box-icon name='dots-horizontal-rounded'></box-icon>    {/*사이드바 추가*/}
-                                <span className="links_name">list_plus</span>
-                            </a>
-                        </li>
+      <div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar" class="active">
+        <ul class="list-unstyled components mb-5">
+          <li class="active">
+            <span class="fa fa-home"></span> Project1
+          </li>
+          <li>
+              <span class="fa fa-user"></span> Project2
+          </li>
+          <li>
+            <span class="fa fa-sticky-note"></span> Project3
+          </li>
+          <li>
+            <span class="fa fa-cogs"></span> Project4
+          </li>
+          <li>
+            <span class="fa fa-paper-plane"></span> Project5
+          </li>
+        </ul>
+</nav>
+       
+		</div>
 
-                    </ul>
-                
 
-                </div>
-
-            </div>
 
 
     );
 
 
 }
-export default FileList;
+export default Sidebar;
