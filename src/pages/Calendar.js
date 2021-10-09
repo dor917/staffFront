bc527571c814583265707b29480b8dd250e60fbc
shinker1002 <shinker1002@naver.com>
2021-10-09 23:53:10 +0900

    Calendar.js

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,110 +1,48 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../css/Calendar.css'
+import React, {Component} from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import axios from 'axios';
 
+//import 'main.css 파일이 위치한 경로'
 
-function Calendar() {
+class Calendar extends Component {
 
-  var gapi = window.gapi
-  /* 
-    Update with your own Client Id and Api key 
-  */
-  var CLIENT_ID = "708830538682-ctmnc0q3pm1mn1inkd0a44u35e20rrj2.apps.googleusercontent.com"
-  var API_KEY = "AIzaSyAfwWKavRAwpD4yl-VPnrOhK-AshZZVLQY"
-  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
-  var SCOPES = "https://www.googleapis.com/auth/calendar.events"
+  state = {}
 
-  const handleClick = () => {
-    gapi.load('client:auth2', () => {
-      console.log('loaded client')
+  componentDidMount() {
+    this._getEvents();
+  }
 
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES,
-      })
+  _getEvents = async () => {
+    const events = await this._axiosEvents();
+    this.setState({
+      events
+    })  
+  }   
 
-      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
-
-      gapi.auth2.getAuthInstance().signIn()
-      .then(() => {
-        
-        var event = {
-          'summary': 'Awesome Event!',
-          'location': '800 Howard St., San Francisco, CA 94103',
-          'description': 'Really great refreshments',
-          'start': {
-            'dateTime': '2020-06-28T09:00:00-07:00',
-            'timeZone': 'America/Los_Angeles'
-          },
-          'end': {
-            'dateTime': '2020-06-28T17:00:00-07:00',
-            'timeZone': 'America/Los_Angeles'
-          },
-          'recurrence': [
-            'RRULE:FREQ=DAILY;COUNT=2'
-          ],
-          'attendees': [
-            {'email': 'lpage@example.com'},
-            {'email': 'sbrin@example.com'}
-          ],
-          'reminders': {
-            'useDefault': false,
-            'overrides': [
-              {'method': 'email', 'minutes': 24 * 60},
-              {'method': 'popup', 'minutes': 10}
-            ]
-          }
-        }
-
-        var request = gapi.client.calendar.events.insert({
-          'calendarId': 'primary',
-          'resource': event,
-        })
-
-        request.execute(event => {
-          console.log(event)
-          window.open(event.htmlLink)
-        })
-        
-
-        /*
-            Uncomment the following block to get events
-        */
-
-        
-        // get events
-        gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
-        }).then(response => {
-          const events = response.result.items
-          console.log('EVENTS: ', events)
-        })
-        
-    
-
-      })
-    })
+  _axiosEvents = () => {
+    return axios.get('/test')
+      .then(res => res.data)
   }
 
 
-  return (
-    <div className="Calendar">
-      <header className="Calendar-header">
-        <img src={logo} className="Calendar-logo" alt="logo" />
-        <p>Click to add event to Google Calendar</p>
-        <p style={{fontSize: 18}}>Uncomment the get events code to get events</p>
-        <p style={{fontSize: 18}}>Don't forget to add your Client Id and Api key</p>
-        <button style={{width: 100, height: 50}} onClick={handleClick}>Add Event</button>
-      </header>
-    </div>
-  );
+  render(){
+
+    let {events} = this.state;
+
+    return (
+      <div className="App">
+        {events ? 
+        <FullCalendar 
+          defaultView="dayGridMonth" 
+          plugins={[ dayGridPlugin ]}
+          events={this.state.events}
+        /> :
+        'loading'
+      }
+      </div>
+    );
+  }
 }
 
 export default Calendar;
