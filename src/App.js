5707d29d676a18336c54ae60a45c40ff8698bfd2
donha <donha917@naver.com>
2021-10-27 18:41:02 +0900

    code read view

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Login from './pages/Login.js';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-   // IP주소 변수 선언
-   const [ip, setIp] = useState('');
+  //  // IP주소 변수 선언
+  //  const [ip, setIp] = useState('');
 
-   // IP주소 값을 설정합니다.
-   function callback(data) {
-     setIp(data);
-   }
+  //  // IP주소 값을 설정합니다.
+  //  function callback(data) {
+  //    setIp(data);
+  //  }
  
-   // 첫번째 렌더링을 다 마친 후 실행합니다.
-   useEffect(
-     () => {
-       // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
-       customAxios('/ip', callback);
-     }, []
-   );
+  //  // 첫번째 렌더링을 다 마친 후 실행합니다.
+  //  useEffect(
+  //    () => {
+  //      // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
+  //      customAxios('/ip', callback);
+  //    }, []
+  //  );
   
   return (
     <div>
---
+++ src/pages/CodeView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../js/CodeView.fileRead.js"
+
+
+function CodeView() {
+   return (
+      <div className="codeview-contaniar">
+     </div>
+   );
+}
+export default CodeView;
