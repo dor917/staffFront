d8e2afa40637c0580c831fa46e502636baee56d7
donha <donha917@naver.com>
2021-11-07 22:22:38 +0900

    codeview change

--- src/pages/CodeView.js
+++ src/pages/CodeView.js
@@ -1,67 +1,192 @@
 import React from 'react';
-import "../js/CodeView.fileRead.js"
-import "../css/CodeView.css"
+import '../js/CodeView.fileRead.js';
+import '../css/CodeView.css';
 
 function CodeView() {
    return (
-      <div className="codeview-contaniar">
-         <table class="highlight tab-size js-file-line-container" data-tab-size="8" data-paste-markdown-skip>
-            <tbody>
-               <tr>
-                  <td id="L1" class="blob-num js-line-number" data-line-number="1">1</td>
-                  <td id="LC1" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+      <div
+         data-target="readme-toc.content"
+         className="Box mt-3 position-relative"
+      >
+         <div className="Box-header py-2 pr-2 d-flex flex-shrink-0 flex-md-row flex-items-center">
+            <div class="text-mono f6 flex-auto pr-3 flex-order-2 flex-md-order-1">
+               138 lines (126 sloc)
+               <span class="file-info-divider"></span>
+               6.19 KB
+            </div>
+            </div>
+            <div class="js-check-bidi blob-code-content">
+               <table
+                  className="highlight tab-size js-file-line-container"
+                  data-tab-size="8"
+                  data-paste-markdown-skip
+               >
+                  <tbody>
+                     <tr>
+                        <td
+                           id="L1"
+                           className="blob-num js-line-number"
+                           data-line-number="1"
+                        >
+                           1
+                        </td>
+                        <td
+                           id="LC1"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L2" class="blob-num js-line-number" data-line-number="2">2</td>
-                  <td id="LC2" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L2"
+                           className="blob-num js-line-number"
+                           data-line-number="2"
+                        >
+                           2
+                        </td>
+                        <td
+                           id="LC2"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L3" class="blob-num js-line-number" data-line-number="3">3</td>
-                  <td id="LC3" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L3"
+                           className="blob-num js-line-number"
+                           data-line-number="3"
+                        >
+                           3
+                        </td>
+                        <td
+                           id="LC3"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L4" class="blob-num js-line-number" data-line-number="4">4</td>
-                  <td id="LC4" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L4"
+                           className="blob-num js-line-number"
+                           data-line-number="4"
+                        >
+                           4
+                        </td>
+                        <td
+                           id="LC4"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L5" class="blob-num js-line-number" data-line-number="5">5</td>
-                  <td id="LC5" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L5"
+                           className="blob-num js-line-number"
+                           data-line-number="5"
+                        >
+                           5
+                        </td>
+                        <td
+                           id="LC5"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L6" class="blob-num js-line-number" data-line-number="6">6</td>
-                  <td id="LC6" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L6"
+                           className="blob-num js-line-number"
+                           data-line-number="6"
+                        >
+                           6
+                        </td>
+                        <td
+                           id="LC6"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L7" class="blob-num js-line-number" data-line-number="7">7</td>
-                  <td id="LC7" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L7"
+                           className="blob-num js-line-number"
+                           data-line-number="7"
+                        >
+                           7
+                        </td>
+                        <td
+                           id="LC7"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L8" class="blob-num js-line-number" data-line-number="8">8</td>
-                  <td id="LC8" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L8"
+                           className="blob-num js-line-number"
+                           data-line-number="8"
+                        >
+                           8
+                        </td>
+                        <td
+                           id="LC8"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L9" class="blob-num js-line-number" data-line-number="9">9</td>
-                  <td id="LC9" class="blob-code blob-code-inner js-file-line">code</td>
-               </tr>
+                     <tr>
+                        <td
+                           id="L9"
+                           className="blob-num js-line-number"
+                           data-line-number="9"
+                        >
+                           9
+                        </td>
+                        <td
+                           id="LC9"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           code
+                        </td>
+                     </tr>
 
-               <tr>
-                  <td id="L10" class="blob-num js-line-number" data-line-number="10">10</td>
-                  <td id="LC10" class="blob-code blob-code-inner js-file-line"> code</td>
-               </tr>
-               
-
-               
-            </tbody>
-         </table>
-     </div>
+                     <tr>
+                        <td
+                           id="L10"
+                           className="blob-num js-line-number"
+                           data-line-number="10"
+                        >
+                           10
+                        </td>
+                        <td
+                           id="LC10"
+                           className="blob-code blob-code-inner js-file-line"
+                        >
+                           {' '}
+                           code
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+            
+         </div>
+      </div>
    );
 }
 export default CodeView;
